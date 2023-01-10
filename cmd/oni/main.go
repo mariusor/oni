00a2ff7d74e953e6283f513ea57c4cc4b31a636b
Marius Orcsik <marius@federated.id>
2023-01-10 19:43:21 +0100

    Added path and listen as flags, using argument as IRI for main actor

--- cmd/oni/main.go
+++ cmd/oni/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"oni"
 	"os"
 	"path/filepath"
@@ -23,24 +24,33 @@ var dataPath = func() string {
 	return filepath.Join(dh, "oni")
 }()
 
+var listen string
+var path string
+
 func main() {
+	flag.StringVar(&listen, "listen", "127.0.0.1:60123", "Listen socket")
+	flag.StringVar(&path, "path", dataPath, "Path for ActivityPub storage")
+	flag.Parse()
+
+	url := "https://oni.local"
+	if flag.NArg() > 0 {
+		url = flag.Arg(0)
+	}
+
 	if build, ok := debug.ReadBuildInfo(); ok && build.Main.Version != "" {
 		oni.Version = build.Main.Version
 	}
 
 	// TODO(marius): add command line arguments for:
-	//   * the IRI of the main actor
-	//   * for the listen string
 	//   * for the path to load from(?). Should we use XDG_DATA
-	iri := vocab.IRI("https://oni.local")
 
 	log := lw.Dev()
 
 	err := oni.Oni(
 		oni.WithLogger(log),
-		oni.LoadActor(iri),
-		oni.WithStoragePath(dataPath),
-		oni.ListenOn("127.0.5.1:60123"),
+		oni.LoadActor(vocab.IRI(url)),
+		oni.WithStoragePath(path),
+		oni.ListenOn(listen),
 	).Run(context.Background())
 
 	if err != nil {
