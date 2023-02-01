66c8c3ed7670369d6dbbec32948d68daa7a0d4c8
Marius Orcsik <marius@federated.id>
2023-02-01 19:52:42 +0100

    Show the basic contents of a user's collection

---
+++ src/ts/activity-pub-create.jsx
@@ -0,0 +1,43 @@
+import {css, html} from "lit";
+import {ActivityPubObject} from "./activity-pub-object";
+import {until} from "lit-html/directives/until.js";
+import {unsafeHTML} from "lit-html/directives/unsafe-html.js";
+
+export class ActivityPubCreate extends ActivityPubObject {
+    static styles = css`
+    :host { color: var(--fg-color); }
+    `;
+
+    constructor() {
+        super();
+    }
+
+    async renderActor() {
+        const act = await this.load('actor');
+        return html`<oni-natural-language-value>${act.preferredUsername}</oni-natural-language-value>`
+    }
+
+    async renderObject() {
+        const ob = await this.load('object');
+
+        switch (ob.type) {
+            case 'Image':
+                const id = ob.id;
+                return html`<img src=${id}/>`;
+            case 'Note':
+                const cont = ob.content;
+                return html`<oni-object it=${JSON.stringify(ob)}>${unsafeHTML(cont)}</oni-object>`;
+        }
+        return '';
+    }
+
+    render() {
+        const published = this.it.hasOwnProperty('published') ?
+            html`at <time datetime=${this.published()}>${this.published()}</time> ` :
+            '';
+        return html`<div>
+            Published ${published}by ${until(this.renderActor())}<br/>
+            ${until(this.renderObject(), "loading...")}
+        </div>`
+    }
+}
--- src/ts/main.jsx
+++ src/ts/main.jsx
@@ -1,11 +1,13 @@
 import {ActivityPubPerson} from "./activity-pub-person";
 import {CollectionLink} from "./collection-link";
 import {NaturalLanguageValue} from "./natural-language-value";
+import {ActivityPubCreate} from "./activity-pub-create";
 import {OnReady, rgb} from "./utils";
 
 customElements.define('oni-natural-language-value', NaturalLanguageValue);
 customElements.define('oni-person', ActivityPubPerson);
 customElements.define('oni-collection-link', CollectionLink);
+customElements.define('oni-create', ActivityPubCreate);
 
 OnReady(function () {
     document.querySelectorAll(":root").forEach((root) => {
