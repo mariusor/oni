df687580003cc33c7e017c65b9b30dbfc30b1f3b
Marius Orcsik <marius@federated.id>
2023-03-09 16:34:13 +0100

    Improve display of objects

--- src/js/activity-pub-note.jsx
+++ src/js/activity-pub-note.jsx
@@ -3,7 +3,26 @@ import {ActivityPubObject} from "./activity-pub-object";
 import {when} from "lit-html/directives/when.js";
 
 export class ActivityPubNote extends ActivityPubObject {
-    static styles = [css``, ActivityPubObject.styles];
+    static styles = [css`
+    article {
+        display: flex;
+        flex-direction: column;
+    }
+    article > * {
+        margin: .1rem;
+    }
+    article header h2 {
+        padding: 0 .1rem;
+        margin: 0;
+        font-size: 1.2rem;
+    }
+    article header {
+        align-self: start;
+    }
+    article footer {
+        align-self: end;
+    }
+    `, ActivityPubObject.styles];
 
     static properties = {
         it: {type: Object},
@@ -15,11 +34,14 @@ export class ActivityPubNote extends ActivityPubObject {
 
     render() {
         const summary = this.summary();
-        return html`${this.renderMetadata()}
-        ${when(summary,
-            () => html`<strong><oni-natural-language-values it=${JSON.stringify(summary)}></oni-natural-language-values></strong><br/>`)
+        console.debug(`summary: `, summary.length)
+        return html`<article>
+        ${when(summary.length > 0,
+            () => html`<header><h2><oni-natural-language-values it=${JSON.stringify(summary)}></oni-natural-language-values></h2></header>`)
         }
         <oni-natural-language-values it=${JSON.stringify(this.content())}></oni-natural-language-values>
-        ${this.renderAttachment()}`;
+        <aside>${this.renderAttachment()}</aside>
+        <footer>${this.renderMetadata()}</footer>
+        </article>`;
     }
 }
