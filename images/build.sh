e087dbdd3e6bfaa6be2cd5891b6a11ff16e82ad6
Marius Orcsik <marius@federated.id>
2023-06-14 13:11:49 +0200

    Adding images

---
+++ images/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#set -x
+
+_workdir=${1:-../}
+_image_name=${2:-oni/builder}
+
+_context=$(realpath "${_workdir}")
+
+_builder=$(buildah from docker.io/library/golang:1.20)
+
+buildah config --env GO111MODULE=on "${_builder}"
+buildah config --env GOWORK=off "${_builder}"
+
+buildah copy --ignorefile "${_context}/.containerignore" --contextdir "${_context}" "${_builder}" "${_context}" /go/src/app
+
+buildah config --workingdir /go/src/app "${_builder}"
+
+buildah run "${_builder}" make download && go mod vendor
+
+buildah commit "${_builder}" "${_image_name}"
---
+++ images/image.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+#set -x
+
+_environment=${ENV:-dev}
+_hostname=${HOSTNAME:-oni}
+_listen_port=${PORT:-5668}
+_storage=${STORAGE:-all}
+_version=${VERSION:-HEAD}
+
+_image_name=${1:-oni:"${_environment}-${_storage}"}
+_build_name=${2:-localhost/oni/builder}
+
+_builder=$(buildah from "${_build_name}":latest)
+
+if [[ -z ${_builder} ]]; then
+    echo "Unable to find builder image: ${_build_name}"
+    exit 1
+fi
+
+echo "Building image ${_image_name} for host=${_hostname} env:${_environment} storage:${_storage} version:${_version} port:${_listen_port}"
+
+buildah run "${_builder}" make ENV="${_environment}" STORAGE="${_storage}" VERSION="${_version}" all
+buildah run "${_builder}" make -C images "${_hostname}.pem"
+
+_image=$(buildah from gcr.io/distroless/static:latest)
+
+buildah config --env ENV="${_environment}" "${_image}"
+buildah config --env HOSTNAME="${_hostname}" "${_image}"
+buildah config --env LISTEN=:"${_listen_port}" "${_image}"
+buildah config --env "KEY_PATH=/etc/ssl/certs/${_hostname}.key" "${_image}"
+buildah config --env "CERT_PATH=/etc/ssl/certs/${_hostname}.crt" "${_image}"
+buildah config --env HTTPS=true "${_image}"
+buildah config --env STORAGE="${_storage}" "${_image}"
+
+buildah config --port "${_listen_port}" "${_image}"
+
+buildah config --volume /storage "${_image}"
+
+buildah copy --from "${_builder}" "${_image}" /go/src/app/bin/* /bin/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/images/*.key /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/images/*.crt /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/app/images/*.pem /etc/ssl/certs/
+
+buildah config --entrypoint '["/bin/oni"]' "${_image}"
+
+# commit
+buildah commit "${_image}" "${_image_name}"
