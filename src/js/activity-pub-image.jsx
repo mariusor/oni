6841c35d68fc2c82963c46dc73cb5e78209950b2
Marius Orcsik <marius@federated.id>
2024-09-09 12:13:50 +0200

    Improve image and alt text display

--- src/js/activity-pub-image.jsx
+++ src/js/activity-pub-image.jsx
@@ -8,11 +8,35 @@ export class ActivityPubImage extends ActivityPubObject {
         img {
             border-radius: .4rem;
             border: 1px solid var(--accent-color);
+            max-width: 100%;
+            height: auto;
         }
         img.small {
             max-width: 1rem;
             max-height: 1rem;
             vertical-align: text-top;
+        }
+        figure {
+            position: relative;
+            max-width: fit-content;
+        }
+        figcaption {
+            position: absolute;
+            padding: 1rem;
+            display: flex;
+            align-items: start;
+        }
+        details {
+            cursor: pointer;
+            font-size: .8em;
+            background-color: color-mix(in srgb, black, transparent 60%);
+            padding: .1rem .4rem;
+            border-radius: .4rem;
+        }
+        summary {
+            list-style-type: none;
+            font-variant: small-caps;
+            font-weight: bold;
         }
         `, ActivityPubObject.styles];
 
@@ -26,17 +50,39 @@ export class ActivityPubImage extends ActivityPubObject {
         return alt.innerText.trim();
     }
 
-    render() {
+    renderInline() {
         let src = this.it.iri();
         if (!src) {
             src = this.it.getUrl();
         }
         const alt = this.renderAltText();
-        return html`<img src=${src ?? nothing} title="${alt}" alt="${alt}" class="${classMap({"small": this.inline})}"/>
-        ${when(
-            this.inline,
-                () => nothing,
-                () => html`<footer>${this.renderMetadata()}</footer>`
-        )}`;
+        return html`<img src=${src ?? nothing} title="${alt}" alt="${alt}" class="small""/>`;
+    }
+
+    render() {
+        let src = this.it.iri();
+        if (!src) {
+            src = this.it.getUrl();
+        }
+        if (this.inline) {
+            return this.renderInline();
+        }
+        const alt = this.renderAltText();
+        return html`
+                <figure>
+                    ${when(alt.length > 0,
+                            () => html`
+                                <figcaption>
+                                    <details>
+                                        <summary>alt</summary>
+                                        ${alt}
+                                    </details>
+                                </figcaption>`,
+                            () => nothing
+                    )}
+                    <img src=${src ?? nothing} title="${alt}" alt="${alt}" />
+                </figure>
+                <footer>${this.renderMetadata()}</footer>
+        `;
     }
 }
