5e3eca21b6f9b388d15a91e0baefb92fea02d3e9
Marius Orcsik <marius@federated.id>
2025-12-13 15:22:23 +0100

    Use new method for building image with buildah

--- Makefile
+++ Makefile
@@ -6,7 +6,9 @@ MAKEFLAGS += --warn-undefined-variables
 MAKEFLAGS += --no-builtin-rules
 
 PROJECT_NAME ?= oni
+APP_HOSTNAME ?= $(PROJECT_NAME)
 ENV ?= dev
+VERSION ?= HEAD
 
 LDFLAGS ?= -X git.sr.ht/~mariusor/oni.Version=$(VERSION)
 BUILDFLAGS ?= -a -ldflags '$(LDFLAGS)' -tags "$(TAGS)"
@@ -30,11 +32,6 @@ TAGS := $(ENV)
 export CGO_ENABLED=0
 export GOEXPERIMENT=greenteagc
 
-ifneq ($(ENV), dev)
-	LDFLAGS += -s -w -extldflags "-static"
-	BUILDFLAGS += -trimpath
-endif
-
 ifeq ($(shell git describe --always > /dev/null 2>&1 ; echo $$?), 0)
 	BRANCH=$(shell git rev-parse --abbrev-ref HEAD | tr '/' '-')
 	HASH=$(shell git rev-parse --short HEAD)
@@ -44,26 +41,36 @@ ifeq ($(shell git describe --tags > /dev/null 2>&1 ; echo $$?), 0)
 	VERSION ?= $(shell git describe --tags | tr '/' '-')
 endif
 
+ifneq ($(ENV), dev)
+	LDFLAGS += -s -w -extldflags "-static"
+	BUILDFLAGS += -trimpath
+endif
+
 BUILD := $(GO) build $(BUILDFLAGS)
 TEST := $(GO) test $(BUILDFLAGS)
 
-.PHONY: all assets test coverage download clean
+.PHONY: all assets cert clean test coverage download help
+
+.DEFAULT_GOAL := help
+
+help: ## Help target that shows this message.
+	@sed -rn 's/^([^:]+):.*[ ]##[ ](.+)/\1:\2/p' $(MAKEFILE_LIST) | column -ts: -l2
 
 all: $(PROJECT_NAME) $(PROJECT_NAME)ctl
 
-download: go.sum
+download: go.sum ## Downloads dependencies and tidies the go.mod file.
 
 go.sum: go.mod
 	$(GO) mod tidy
 
-$(PROJECT_NAME): bin/$(PROJECT_NAME)
+$(PROJECT_NAME): bin/$(PROJECT_NAME) ## Builds the ONI service binary.
 bin/$(PROJECT_NAME): go.sum $(ONI_BIN_SOURCES) $(GO_SOURCES) assets
 	$(BUILD) -o $@ ./cmd/oni
 ifneq (,$(findstring $(ENV), "prod qa"))
 	$(UPX) -q --mono --no-progress --best $@ || true
 endif
 
-$(PROJECT_NAME)ctl: bin/$(PROJECT_NAME)ctl
+$(PROJECT_NAME)ctl: bin/$(PROJECT_NAME)ctl ## Builds the ONI control binary.
 bin/$(PROJECT_NAME)ctl: go.sum $(CTL_BIN_SOURCES) $(GO_SOURCES)
 	$(BUILD) -o $@ ./cmd/ctl
 ifneq (,$(findstring $(ENV), "prod qa"))
@@ -73,7 +80,7 @@ endif
 yarn.lock:
 	$(YARN) install
 
-assets: yarn.lock
+assets: yarn.lock ## Builds the static assets.
 	go generate -v assets.go
 
 static/main.js: $(TS_SOURCES) yarn.lock
@@ -106,3 +113,8 @@ test: go.sum ## Run unit tests for the service.
 coverage: TEST_TARGET := .
 coverage: TEST_FLAGS += -covermode=count -coverprofile $(PROJECT_NAME).coverprofile
 coverage: test
+
+cert: bin/$(APP_HOSTNAME).pem ## Create a certificate.
+bin/$(APP_HOSTNAME).pem: bin/$(APP_HOSTNAME).key bin/$(APP_HOSTNAME).crt
+bin/$(APP_HOSTNAME).key bin/$(APP_HOSTNAME).crt:
+	./images/gen-certs.sh ./bin/$(APP_HOSTNAME)
--- images/image.sh
+++ images/image.sh
@@ -1,26 +1,47 @@
 #!/usr/bin/env sh
 
-#set -x
+set -e
+_context=$(realpath "../")
 
 _environment=${ENV:-dev}
 _hostname=${APP_HOSTNAME:-oni}
 _listen_port=${PORT:-5668}
-_storage=${STORAGE:-all}
 _version=${VERSION:-HEAD}
 
-_image_name=${1:-oni:"${_environment}-${_storage}"}
-_build_name=${2:-localhost/oni/builder}
+_image_name=${1:-oni:"${_environment}"}
 
-_builder=$(buildah from "${_build_name}":latest)
-if [ -z "${_builder}" ]; then
-    echo "Unable to find builder image: ${_build_name}"
-    exit 1
-fi
+HOST_GOCACHE=$(go env GOCACHE)
+HOST_GOMODCACHE=$(go env GOMODCACHE)
 
-echo "Building image ${_image_name} for host=${_hostname} env:${_environment} storage:${_storage} version:${_version} port:${_listen_port}"
+GOCACHE=/root/.cache/go-build
+GOMODCACHE=/go/pkg/mod
 
-buildah run "${_builder}" make ENV="${_environment}" STORAGE="${_storage}" VERSION="${_version}" all
-buildah run "${_builder}" ./images/gen-certs.sh "${_hostname}"
+_builder=$(buildah from docker.io/library/golang:1.25-alpine)
+
+buildah run "${_builder}" /sbin/apk update
+buildah run "${_builder}" /sbin/apk add yarn make bash openssl upx
+
+buildah config --env GO111MODULE=on "${_builder}"
+buildah config --env GOWORK=off "${_builder}"
+buildah config --env YARN=yarnpkg "${_builder}"
+buildah config --env "GOCACHE=${GOCACHE}" "${_builder}"
+buildah config --env "GOMODCACHE=${GOMODCACHE}" "${_builder}"
+
+buildah config --workingdir /go/src/app "${_builder}"
+
+echo "Building image ${_image_name} for host=${_hostname} env:${_environment} version:${_version} port:${_listen_port}"
+
+buildah run \
+    --mount="type=bind,rw,source=${HOST_GOCACHE},destination=${GOCACHE}" \
+    --mount="type=bind,rw,source=${HOST_GOMODCACHE},destination=${GOMODCACHE}" \
+    --mount="type=bind,rw,source=${_context},destination=/go/src/app" \
+    --mount=type=cache,rw,id=bin,target=/go/src/app/bin "${_builder}" \
+    make ENV="${_environment}" VERSION="${_version}" FEDBOX_HOSTNAME="${_hostname}" clean all cert
+
+# copy binaries from cache to builder container fs
+buildah run \
+    --mount=type=cache,rw,id=bin,target=/tmp/bin "${_builder}" \
+    cp -ri /tmp/bin /go/src/
 
 _image=$(buildah from gcr.io/distroless/static:latest)
 
@@ -30,16 +51,15 @@ buildah config --env LISTEN=:"${_listen_port}" "${_image}"
 buildah config --env KEY_PATH="/etc/ssl/certs/${_hostname}.key" "${_image}"
 buildah config --env CERT_PATH="/etc/ssl/certs/${_hostname}.crt" "${_image}"
 buildah config --env HTTPS=true "${_image}"
-buildah config --env STORAGE="${_storage}" "${_image}"
 
 buildah config --port "${_listen_port}" "${_image}"
 
 buildah config --volume /storage "${_image}"
 
-buildah copy --from "${_builder}" "${_image}" /go/src/app/bin/* /bin/
-buildah copy --from "${_builder}" "${_image}" "/go/src/app/${_hostname}.key" /etc/ssl/certs/
-buildah copy --from "${_builder}" "${_image}" "/go/src/app/${_hostname}.crt" /etc/ssl/certs/
-buildah copy --from "${_builder}" "${_image}" "/go/src/app/${_hostname}.pem" /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" /go/src/bin/* /bin/
+buildah copy --from "${_builder}" "${_image}" "/go/src/bin/${_hostname}.key" /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" "/go/src/bin/${_hostname}.crt" /etc/ssl/certs/
+buildah copy --from "${_builder}" "${_image}" "/go/src/bin/${_hostname}.pem" /etc/ssl/certs/
 
 buildah config --workingdir / "${_image}"
 buildah config --entrypoint "$(printf '["/bin/oni", "--listen", ":%s", "--path", "/storage"]' "${_listen_port}")" "${_image}"
